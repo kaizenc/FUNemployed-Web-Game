0a077cfd5767db4d3da417f95baafefda637f2a9
Kaizen Castanos <kaizenc98@gmail.com>
2018-01-08 22:35:37 -0500

    Basic done: Needs refactoring and redesigning

---
+++ script.js
@@ -0,0 +1,166 @@
+//Contains actual game functions
+
+var codePause= true;
+
+function Turn(boss){
+	console.log(getJob()); //display job
+	for (p=0;p<players.length;p++){
+	  if (players[p] == boss){ continue; }
+		for (i=0;i<4;i++){
+			console.log(getQual());
+			while(codePause){};
+		}
+	}
+	//increase score
+	winner = "Player1";
+	incScore(winner);
+}
+
+function show(divName){
+	$("#MainPage").hide();
+	$("#GamePage").hide();
+	$("#ScorePage").hide();
+	$("#" + divName).show();
+}
+
+function startGame(){
+	numPlayers = $("#numOfPlayers").val();
+	scores = {};
+	for(i=0;i<numPlayers;i++){
+		playername = $("input[name='pName" + i + "']").val();
+		if (playername == ""){			
+			scores["Player" + (i + 1)] = 0;
+		}else{			
+			scores[playername] = 0;
+		}
+	}
+	//player1n = $("input[name='pName1']").val(); //gets playerName
+	show("GamePage");
+	roundStart();
+}
+
+function roundStart(){//Sets up the html
+	var bossName = pName(currentBoss);
+	$("#GamePage h1").text("Boss: " + bossName);
+	$("#GamePage h2:first").text("Job: " + getJob());
+	var table = $("#pastQuals table");
+	for(i=0;i<numPlayers;i++){
+		if (i==currentBoss){
+			continue;
+		}
+		table.append("<tr><td>" + Object.keys(scores)[i] + ": " + "</td></tr>");
+	}
+	if(currentBoss == 0){
+		$("#currentQuals h3").text("Interviewee: " + pName(1));
+	}else{
+		$("#currentQuals h3").text("Interviewee: " + pName(0));
+	}
+	$("#currentQuals h2").text(getQual());
+	$("#currentQuals h4").text("");
+}
+function nextQual(){
+	if(qualCounter != 0){
+		$("#currentQuals h4").append(" | " + $("#currentQuals h2").text());
+	}
+	$("#currentQuals h2").text(getQual());
+	qualCounter++;
+	if(qualCounter == 4){
+		var button = $("#currentQuals button");
+		button.text("Next Employee");
+		button.attr("onclick", "nextEmployee();");
+	}
+}
+function nextEmployee(){
+	//Store qualifications in table
+	var temp_quals = $("#currentQuals h4").text().split(" | ");
+	var table_row = $("#pastQuals table tr:nth-child(" + currentPlayer + ")");
+	for(i=1;i<temp_quals.length;i++){
+		table_row.append("<td>"+temp_quals[i]+"</td>");
+	}
+	table_row.append("<td>"+$("#currentQuals h2").text()+"</td>");
+	//Reset counter, button, and other stuff
+	$("#currentQuals h4").text("");
+	qualCounter = 0;
+	var button = $("#currentQuals button");
+	button.text("Next Qualification");
+	button.attr("onclick", "nextQual();");
+
+	currentPlayer++;
+	//Gets the next qualification (if not finished with turn)
+	if(currentPlayer < numPlayers){
+		$("#currentQuals h3").text("Interviewee: " + pName(currentPlayer));
+		nextQual();
+		return;
+	}
+	//else, show the other thing
+	$("#currentQuals").hide();
+	setupPickWinner();
+	$("#pickWinner").show();
+}
+function setupPickWinner(){
+	var div = $("#pickWinner");
+	for(i=0;i<numPlayers;i++){
+		if (i==currentBoss){
+			continue;
+		}
+		var string = "<button onclick=";
+		string+='"pickedWinner(';
+		string+="'";
+		string+=Object.keys(scores)[i];
+		string+="'";
+		string+=');">';
+		string+=Object.keys(scores)[i];
+		string+="</button>"
+		div.append(string);
+	}
+}
+function pickedWinner(winner){
+	var string = winner + " is the new ";
+	string+=$("#GamePage h2:first").text().slice(5);
+	string+="!";
+	$("#ScorePage h1").text(string);
+	scores[winner]++;
+	var scoreTable = $("#ScorePage h2");
+	for(i=numPlayers-1;i>-1;i--){
+		string = "<h3>" + pName(i) + ": " + scores[pName(i)] + "</h3>";
+		scoreTable.after(string);
+	}
+	show("ScorePage");
+}
+function changeForm(){
+	var selector = $("#numOfPlayers").val();
+	for(i=0;i<6;i++){
+		if(i<selector){
+			$("input[name='pName" + i + "']").show();
+		}else{
+			$("input[name='pName" + i + "']").hide();
+		}
+	}
+}
+function softReset(){
+	currentBoss = nextBoss();
+	$("#ScorePage h3").remove(); //resets scoreboard
+	$("#pickWinner button").remove(); //resets pickWinner
+	$("#pastQuals table tr").remove(); //resets table rows
+	$("#currentQuals").show(); //shows the game
+	$("#pickWinner").hide(); //hides the pick section
+	currentPlayer=1;
+	roundStart();
+	show("GamePage");
+}
+ 
+
+
+/****How a Turn Works
+One player is picked to be the job
+One of the other players is picked to be the current job...person
+Pick up to 4 qualifications at a time
+Job picks a winner
+then loop back
+Have either an end button or a number of rounds
+
+Turn will be saved in a function
+#rounds is a for loop
+end button is a while loop and a thing
+write a reset function as well
+*/
---
+++ vars.js
@@ -0,0 +1,40 @@
+/**Used to store cards & scores/players & Basic Functions**/
+
+var jobs = ['Bartender', 'Psychic', 'Celebrity Impersonator ', 'Model ', 'Magician ', 'Author', 'Escort ', 'Queen ', 'Executioner', 'Dominatrix', 'Drill Sergeant', 'Game Show Host', 'Altar Boy', 'School Nurse', 'Motivational Speaker', 'Professional Athlete', 'Intern', 'Mad Scientist', 'Taxi Driver', 'Private Detective', 'B-Movie Actor', 'Fortune Cookie Writer', 'Plumber', 'Singer', 'Mime', 'Super Hero', 'Reality Show Contestant', 'Cheerleader', 'News Anchor', 'Therapist', 'Butler', 'Nanny', 'Tea Party Candidate', 'Archaeologist', 'Proctologist', 'Gym Teacher', 'Porn Star', 'Bounty Hunter', 'Masseuse', 'Televangelist', 'Child Actor', 'Competetive Eater', 'Used Car Salesman', 'Live Action Role Player', 'Stunt Double', 'Pimp ', 'Politician', 'Venture Capitalist', 'Personal Trainer', 'Pirate', 'Priest', 'Lawyer', 'Dictator', 'Flight Attendant', 'Wet Nurse', 'Gangster', 'World Record Holder', 'Gynecologist', 'Ice Cream Maker', 'Housewife', 'Auctioneer', 'TSA Agent', 'Butcher', 'Barista', 'Bachelor', 'Astronaut', 'Super Villain', 'UN Weapons Inspector', 'Secret Agent'];
+var jobs_used = [];
+var quals = ['Spaceship', 'Loose', 'Merit Badge', 'Fishnet Stockings', 'Confession', 'Black Hole', 'Brownies', 'Ambidextrous', 'Bipolar', 'Peg Leg', 'Dragon', 'Six Months Left to Live', 'Jacked Forearms', 'Box Wine', 'Handcuffs', 'K.O.', 'Slimy', 'Social Media Profile', 'Utility Belt', 'Born on the Streets', 'Cesspool', 'Fear', 'Expert', 'Deaf in One Ear', 'Emotionally Hollow', 'Student Loans', 'Box', 'S&M', 'Utterly Alone', 'Grunt', 'Whip', 'Open', 'Affirmative Action', 'Chainsaw', 'Sob Story', 'Secret Identity', 'Deep', 'Decoder Ring', 'Shady', 'Cape', 'Cavity', 'Chocolate', 'Stroke', 'Monocle', 'Split Personality', 'Brass Knuckles', 'Clammy', 'Night Terrors', 'Filter', 'Room to Grow', 'Extra Credit', 'Rock', 'Crabs', 'Uncontrollable Libido', 'The Perfect Alibi', 'Bitches', 'Foot Fetish', 'Boomerang', 'Recess', 'Sneaky', 'Diplomatic Immunity', 'Australian Accent', 'Conviction', 'No Sense of Humor', 'German Accent', 'Self-Respect', 'Grass', 'Minivan', 'Film', 'British Accent', 'False Teeth', 'Effeminate', 'Soundproof Room', 'Spirit', 'Heavy Flow', 'Survival Skills', 'Katana', 'Holy Grail', 'A Dollar', 'Tasteless', 'Cane', 'Piercing', 'Drive', 'Evil Laugh', 'Daddy Issues', 'Sloth', 'Erectile Dysfunction', 'Jackhammer', 'Lust', 'Handlebar Moustache', 'Liberal', 'Chastity Belt', 'Insatiable', 'Gold Cross', 'Fancy Hat', 'Chronic Masturbator', 'Self-Loathing', 'Mormon', 'Red Sea', 'Swear', 'Unstable', 'Million Dollar Smile', 'Bone', 'Nunchucks', 'Grit', 'Speech Impediment', 'Dirty', 'Mind Reader', 'Oedipus Complex', 'Plow', 'Sawed-Off Shotgun', 'Utterly Adorable', 'Cannibal', 'License', "Can't Lie", 'Scissors', 'S.T.D.', 'Field Experience', 'Wings', 'Camera', 'Medicine', 'Sees Dead People', 'Viral', 'Black Belt', 'Calendar Model', 'Soft Voice', 'Treats', 'Night Vision', 'Shame', 'Steroids', 'Genetically Engineered', 'Soft Hands', 'Jet Packs', 'Hook', 'Clinically Depressed', 'Russian Accent', 'Spray', 'Can Defuse Bombs', 'Cavernous', "Can't Feel", 'Screw', 'Scalpel', 'Nothing Left to Lose', 'Happy Ending', 'Topless', 'Pride', 'Envy', 'Communes with Fish', 'Tip', 'Stool', 'Poker Face', 'Soulless', 'Sidekick', 'Cougars', 'Drug Habit', 'Slur', 'Candy', 'Mace', 'Good Times in Tijuana', 'Short Attention Span', 'Spandex', 'Scented Candle', 'Moist', 'Obsessive Compulsive', 'Flaming Sword', 'Indecisive', 'Pyromaniac', 'Apples', 'Scientology', 'Sack', 'Firebreathing', 'Online Dating Profile', 'Three-Piece Suit', 'Shooting Blanks', 'Day Job', 'Crystal Ball', 'Consolation Prize', 'Indian Accent', 'Passive Agressive', 'A.I.', 'Rain', 'Magnum', 'Terrible Things', 'Fifty Tattoos', 'Rum', 'Thug Life', 'Trapped in the Closet', 'Wrath', 'Work Ethic', 'Experiments', 'Sad Childhood', 'Gambling Addiction', 'Conservative', 'Sports Car', 'Walker', 'Trust Fund', "Devil's Advocate", '1%', 'Numb Face', 'Time Machine', 'Wet Dream', 'V Card', 'Saggy', 'Green Card', 'Sexual Tyrannosaurus', 'Hot Dog', 'Jazz Hands', 'Hash', 'Every Problem Ever', 'Ripped', "Associate's Degree", 'Cold Black Heart', 'Raging Alcoholic', 'Claw', 'No Sense of Smell', 'Shed', 'Really Bad Aim', 'Heights', 'Southern Accent', 'Poor Judgement', 'Magic Wand', 'Greed', 'Massage Oil', 'Braces', 'High', 'Patient', 'Beefcake', 'Purple Drank', 'Hoverboard', 'God Complex', 'Slender Frame', 'Swag', 'Invisible Ink', 'Lisp', 'Combover', 'Chain', 'Treasure', 'Disorder', 'Uncontrollable Gas', 'Blunt', 'Dingo Repellent', 'Condoms', 'Gag Reflex', 'Shaved', 'Excuses', 'Decency', 'Sex Den', 'The Antidote', 'Beard', 'Red Light', 'Hot Mess', 'Original Sin', 'Taint', 'Nice Things', 'Hocus Pocus', 'Privilege', 'Camouflage', 'X-ray Vision', 'Nutcracker', 'Balls', 'Fifty Cats', 'Foam Sword', 'Shallow', 'Self-Entitled', 'Snatch', 'Trench Coat', 'Slutty', 'Tramp Stamps', 'Sweat', 'Boats', 'Fairy Dust', 'Red Panda', 'Private Jet', 'Tight Quarters', 'Erotica Collection', 'Furry', 'Italian Accent', 'Sheltered', 'Yoga Pants', 'Club', 'Edible Underwear', 'Brick', 'Telenovela', 'Rapid', 'Buddhism', 'Crack', 'Piece of Cake', 'Package', 'Six-Pack', 'Critical Mass', 'Gluttony', 'Tentacles', 'Wasp', 'Tickle', 'Apocalypse', 'Minion', 'Emotionally Unstable', 'Online Degree', 'Bad Luck', 'Opium Den', 'Cougar', 'El Diablo', 'French Accent', 'Addictive Personality', 'Level', 'Hungover', 'Disability', 'Illiterate', 'Hallucinates', 'Pathological Liar', 'Game Tester', 'Organic', 'Files', 'Captain', 'Burning Bush', 'Wheelchair', 'Trisexual', 'Handlebars', 'Coat Hanger', 'Pound', 'Implants', 'Hears Voices', 'Dungeon'];
+var quals_used = [];
+
+//counters
+var scores = {}; //example, will populate with function later
+var numPlayers = 0;
+var currentBoss = 0;
+var currentPlayer = 1; //tracks which player/employee is going up for quals
+var qualCounter = 1;
+
+function getJob(){
+	if(jobs.length === 0){
+		jobs = jobs.concat(jobs_used);
+		jobs_used = [];
+	}
+	var index = Math.floor(Math.random() * jobs.length); //random var
+	var result = jobs[index]; //returns result later
+	jobs_used = jobs_used.concat(jobs.splice(index, 1)); //puts the jobs into used arr
+	return result; //returns result
+}
+function getQual(){
+	if(quals.length === 0){
+		quals = quals.concat(quals_used);
+		quals_used = [];
+	}
+	var index = Math.floor(Math.random() * quals.length); //random var
+	var result = quals[index]; //returns result later
+	quals_used = quals_used.concat(quals.splice(index, 1)); //puts the quals into used arr
+	return result; //returns result
+}
+function nextBoss(){
+	return (currentBoss+1 == numPlayers)?0:currentBoss+1;
+}
+function pName(x){
+	return Object.keys(scores)[x];
+}
